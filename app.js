91423588367daa9b7b990541eb4c234e85e489b9
c3lm3r <justin.celmer@gmail.com>
2024-07-22 15:41:46 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is a Test and it works!'));
+app.get('/', (req, res) => res.send('Second Test -- IT SHOULD WORK!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
